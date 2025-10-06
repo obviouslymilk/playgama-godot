976b404627d6c1168853a9ec8db7203c3ac588e7
Klim <storozhenko.klim@gmail.com>
2025-10-06 17:05:34 +0300

    refactor(MainButton): оптимизация кода смены стиля

---
+++ global/custom_colors.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name CustomColors
+
+const PRIMARY = Color('9747FF')
+
+const BG_DARK = Color('0D0D0F')
+const BG_PRIMARY = Color('1D1D21')
+const BG_SECONDARY = Color('29292E')
+const BG_TERTIARY = Color('49494C')
--- ui/main_button/main_button.gd
+++ ui/main_button/main_button.gd
@@ -1,51 +1,44 @@
 @tool
 class_name MainButton
-extends Panel
+extends Control
 
-@onready var IconNode = $ContentMargin/Content/Icon
-@onready var LabelNode = $ContentMargin/Content/Label
-@onready var ContentNode = $ContentMargin/Content
+@onready var content_node = $Panel/ContentMargin/Content
+@onready var icon_node = $Panel/ContentMargin/Content/Icon
+@onready var label_node = $Panel/ContentMargin/Content/Label
+@onready var panel_node = $Panel
 
 @export var icon: Texture2D:
-	set(new_image):
-		icon = new_image
-		if IconNode:
-			IconNode.texture = icon
-
+	set(value):
+		icon = value
+		_update_visuals()
 @export var label: String = "":
-	set(new_text):
-		label = new_text
-		if LabelNode:
-			LabelNode.text = label
-
-@export var color: Color = Color.WHITE:
-	set(new_color):
-		color = new_color
-		if ContentNode:
-			ContentNode.modulate = color
-
-@export var route_name: String = ""
-
-func _gui_input(event: InputEvent) -> void:
-	if Engine.is_editor_hint(): return
-
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			Router.redirect(route_name)
+	set(value):
+		label = value
+		_update_visuals()
+@export var color: Color = Color.WHITE
+@export var is_secondary: bool = false;
 
 func _ready() -> void:
-	IconNode.texture = icon;
-	LabelNode.text = label;
-	ContentNode.modulate = color;
+	_update_visuals()
+	_apply_style()
 
-func _notification(what):
-	match what:
-		NOTIFICATION_MOUSE_ENTER:
-			var new_sb := get_theme_stylebox("panel").duplicate()
-			new_sb.bg_color = color
-			add_theme_stylebox_override("panel", new_sb)
+func _on_mouse_entered() -> void:
+	var sb = panel_node.get_theme_stylebox("panel").duplicate()
+	sb.bg_color = color if not is_secondary else Color.WHITE
+	panel_node.add_theme_stylebox_override("panel", sb)
+	content_node.modulate = CustomColors.BG_PRIMARY if not is_secondary else CustomColors.BG_TERTIARY
 
-			ContentNode.modulate = Color('#29292E')
-		NOTIFICATION_MOUSE_EXIT:
-			remove_theme_stylebox_override("panel")
-			ContentNode.modulate = color
+func _on_mouse_exited() -> void:
+	_apply_style()
+
+func _apply_style() -> void:
+	var sb = panel_node.get_theme_stylebox("panel").duplicate()
+	sb.bg_color = CustomColors.BG_PRIMARY if not is_secondary else CustomColors.BG_TERTIARY
+	panel_node.add_theme_stylebox_override("panel", sb)
+	content_node.modulate = color if not is_secondary else Color.WHITE
+
+func _update_visuals() -> void:
+	if icon_node:
+		icon_node.texture = icon
+	if label_node:
+		label_node.text = label
