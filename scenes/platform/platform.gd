dfd5357338dee413cbb10ff1fbae0d02da0f1616
Klim <storozhenko.klim@gmail.com>
2025-10-06 20:12:56 +0300

    feat(Platform): добавляет кнопки действий

--- scenes/platform/platform.gd
+++ scenes/platform/platform.gd
@@ -3,7 +3,7 @@ extends VBoxContainer
 @onready var StatsText = $Margin/ContentPanel/Margin/Content/StatsText
 
 func _ready() -> void:
-	var new_text : String = StatsText.text
+	var new_text: String = StatsText.text
 	new_text = new_text.replace("{id}", Bridge.platform.id)
 	new_text = new_text.replace("{lang}", Bridge.platform.language)
 	new_text = new_text.replace("{payload}", str(Bridge.platform.payload))
@@ -16,5 +16,10 @@ func _ready() -> void:
 
 
 func _on_audio_state_changed(is_enabled: bool) -> void:
-	var new_text : String = StatsText.text
+	var new_text: String = StatsText.text
 	new_text = new_text.replace("{audio}", str(is_enabled))
+
+
+func _on_game_ready_pressed() -> void:
+	Bridge.platform.send_message(Bridge.PlatformMessage.GAME_READY)
+	print("Game Ready отправлено")
